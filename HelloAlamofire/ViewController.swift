65b244e7336a8984ff225b2dd25f887000092955
Sushant Sahu <sushantsahu@Sushants-MacBook-Pro.local>
2017-01-06 11:09:07 +0530

    Initial Commit

---
+++ HelloAlamofire/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloAlamofire
+//
+//  Created by Sushant Sahu on 06/01/17.
+//  Copyright © 2017 Sushant Sahu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
